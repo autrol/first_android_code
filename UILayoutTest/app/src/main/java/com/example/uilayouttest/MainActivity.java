c76da40a29d28d3afc0b39fd3414967085c45e67
zhinian.yang <zhinian.yang@husor.com>
2017-08-02 22:29:35 +0800

    第三章例子

--- UILayoutTest/app/src/main/java/com/example/uilayouttest/MainActivity.java
+++ UILayoutTest/app/src/main/java/com/example/uilayouttest/MainActivity.java
@@ -1,7 +1,13 @@
 package com.example.uilayouttest;
 
+import android.app.Activity;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import static java.security.AccessController.getContext;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.hide();
+        }
+
     }
 }
