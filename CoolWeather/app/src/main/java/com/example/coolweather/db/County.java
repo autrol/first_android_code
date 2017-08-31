62a4777e95bc918408fbcfee6a873936a5a2699f
autrol <389082410@qq.com>
2017-09-01 00:28:20 +0800

    完成遍历省市县三级列表的功能

---
+++ CoolWeather/app/src/main/java/com/example/coolweather/db/County.java
@@ -0,0 +1,49 @@
+package com.example.coolweather.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by yangzhinian on 2017/8/31.
+ */
+
+public class County extends DataSupport {
+    private int id;
+
+    private String countyName;
+
+    private String weatherId;
+
+    private int cityId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getWeatherId() {
+        return weatherId;
+    }
+
+    public String getCountyName() {
+        return countyName;
+    }
+
+    public void setCountyName(String countyName) {
+        this.countyName = countyName;
+    }
+
+    public void setWeatherId(String weatherId) {
+        this.weatherId = weatherId;
+    }
+
+    public int getCityId() {
+        return cityId;
+    }
+
+    public void setCityId(int cityId) {
+        this.cityId = cityId;
+    }
+}
