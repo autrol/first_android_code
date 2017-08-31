257e41b42c4903e2821ffdaefadcfe820b363ab0
autrol <389082410@qq.com>
2017-08-31 23:35:36 +0800

    天气

---
+++ MaterialTest/app/src/main/java/com/example/materialtest/Fruit.java
@@ -0,0 +1,32 @@
+package com.example.materialtest;
+
+/**
+ * Created by yangzhinian on 2017/8/26.
+ */
+
+public class Fruit {
+    private String name;
+
+    private int imageId;
+
+    public Fruit(String name, int imageId) {
+        this.name = name;
+        this.imageId = imageId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getImageId() {
+        return imageId;
+    }
+
+    public void setImageId(int imageId) {
+        this.imageId = imageId;
+    }
+}
